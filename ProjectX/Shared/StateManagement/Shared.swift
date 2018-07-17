df367133e70eab64b6428cf02434d8ecadbae787
Bruce Colby <bruce.colby.bc@gmail.com>
2018-07-17 13:12:03 -0400

    — Added Shared singleton

---
+++ ProjectX/Shared/StateManagement/Shared.swift
@@ -0,0 +1,18 @@
+//
+//  Shared.swift
+//  ProjectX
+//
+//  Created by Bruce Colby on 7/16/18.
+//  Copyright © 2018 AmTrust. All rights reserved.
+//
+
+import Foundation
+import ProjectXCore
+
+class Shared {
+    // MARK: - Properties
+    static let shared = Shared()
+    
+    // MARK: -
+    var user: [User] = []
+}
