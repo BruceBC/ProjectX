1664a10f9d292a1b1b5ec7831e33b81ea56f31b5
Bruce Colby <bruce.colby.bc@gmail.com>
2018-08-14 08:52:15 -0400

    — Added ImageError

---
+++ ProjectXCore/Shared/Errors/ImageError.swift
@@ -0,0 +1,13 @@
+//
+//  ImageError.swift
+//  ProjectXCore
+//
+//  Created by Bruce Colby on 8/14/18.
+//  Copyright © 2018 AmTrust. All rights reserved.
+//
+
+import Foundation
+
+public enum ImageError: Error {
+    case empty(String)
+}
