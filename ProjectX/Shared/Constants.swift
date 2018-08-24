6619788d5fc7b4547ac99d16b8ffa4d24b9e28b5
Bruce Colby <bruce.colby.bc@gmail.com>
2018-08-24 17:56:12 -0400

    — Added SegueIdentifiers to Constants

--- ProjectX/Shared/Constants.swift
+++ ProjectX/Shared/Constants.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+
+enum SegueIdentifiers {
+    static let personDetailViewController     = "PersonDetailViewController"
+    static let showPersonDetailViewController = "showPersonDetailViewController"
+}
