a1af484f3b5d4c9356db5df872777e508f3f1909
Bruce Colby <bruce.colby.bc@gmail.com>
2018-08-31 14:19:29 -0400

    — Added SearchDetailDismissAnimationController (missing animation logic)

---
+++ ProjectX/Search/Transitions/Dismiss/SearchDetailDismissAnimationController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchDetailDismissAnimationController.swift
+//  ProjectX
+//
+//  Created by Bruce Colby on 8/31/18.
+//  Copyright © 2018 AmTrust. All rights reserved.
+//
+
+import UIKit
+
+class SearchDetailDismissAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.6
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        // Do animation stuff here
+    }
+}
